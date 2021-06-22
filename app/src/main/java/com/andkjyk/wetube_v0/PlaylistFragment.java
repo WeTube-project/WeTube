69430664ec7254e54431c034264ad4b4ec2366fb
brothermanbro <wonone0902@gmail.com>
2021-06-23 02:19:34 +0900

    방정보 다 받는거 까지 구현 가능한데 리스트에 표시하는 부분에서 알 수 없는 막힘 발생

--- app/src/main/java/com/andkjyk/wetube_v0/PlaylistFragment.java
+++ app/src/main/java/com/andkjyk/wetube_v0/PlaylistFragment.java
@@ -6,6 +6,7 @@ import android.os.Build;
 import android.os.Bundle;
 
 import androidx.annotation.Nullable;
+import androidx.appcompat.app.AppCompatActivity;
 import androidx.fragment.app.Fragment;
 import androidx.recyclerview.widget.DefaultItemAnimator;
 import androidx.recyclerview.widget.LinearLayoutManager;
@@ -15,21 +16,30 @@ import android.text.Html;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Toast;
 
 import com.andkjyk.wetube_v0.Adapter.PlaylistAdapter;
 import com.andkjyk.wetube_v0.Model.PlaylistItem;
+import com.android.volley.Request;
+import com.android.volley.RequestQueue;
+import com.android.volley.toolbox.JsonObjectRequest;
+import com.android.volley.toolbox.Volley;
 import com.google.android.material.floatingactionbutton.FloatingActionButton;
 import com.google.android.material.snackbar.Snackbar;
+
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
 
 import java.util.ArrayList;
 
 public class PlaylistFragment extends Fragment {
-
+    RequestQueue requestQueue;
     private RecyclerView plRecyclerView;
     private PlaylistAdapter plAdapter;
     private RecyclerView.LayoutManager layoutManager;
     private int ADDPLAYLIST_REQUEST_CODE = 208;
-
+    String videoId, publisher, thumbnailUrl, title;
     private ArrayList<PlaylistItem> plItemList = new ArrayList<>();
     String roomCode;
 
@@ -37,18 +47,21 @@ public class PlaylistFragment extends Fragment {
         // Required empty public constructor
     }
 
+
+
     @Override
     public void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
         super.onActivityResult(requestCode, resultCode, data);
 
         if(data != null){
-            String videoId = data.getStringExtra("s_videoId");
-            String publisher = data.getStringExtra("s_publisher");
-            String thumbnailUrl = data.getStringExtra("s_thumbnailUrl");
-            String title = data.getStringExtra("s_title");
-
+            videoId = data.getStringExtra("s_videoId");
+            publisher = data.getStringExtra("s_publisher");
+            thumbnailUrl = data.getStringExtra("s_thumbnailUrl");
+            title = data.getStringExtra("s_title");
+            postMedia(roomCode, title, publisher,thumbnailUrl, videoId);
             plAdapter.addItem(new PlaylistItem(title, publisher, videoId, thumbnailUrl, roomCode));
             plAdapter.notifyDataSetChanged();
+
         }
     }
 
@@ -76,7 +89,7 @@ public class PlaylistFragment extends Fragment {
                 //startActivity(intent);
             }
         });
-
+        requestQueue = Volley.newRequestQueue(getActivity().getApplicationContext());
         getData();
         plRecyclerView = (RecyclerView) view.findViewById(R.id.rv_playlist);
         plRecyclerView.setHasFixedSize(true);
@@ -90,40 +103,35 @@ public class PlaylistFragment extends Fragment {
     }
 
     private void getData(){
+        String url = "http://3.37.36.38:3000/media";
 
+
+      JsonObjectRequest jsonObjReq = new JsonObjectRequest(Request.Method.GET, url, null,
+                response -> {
+                    try {
+                        int roominfo_size = response.getInt("roominfoSize");
+                        JSONArray roominfoarr = response.getJSONArray("roominfo");
         plItemList.clear();
         ArrayList<String> listPlVideoName = new ArrayList<>();
         ArrayList<String> listPlPublisher = new ArrayList<>();
         ArrayList<String> listPlVideoId = new ArrayList<>();
         ArrayList<String> listPlThumbnailURL = new ArrayList<>();
-
-        for(int i = 0; i < 1; i++){
-            String title = "[놀면 뭐하니?] 유야호가 쏘아 올린 왕의 귀환\uD83E\uDD34 한 클립에 모아보기ㅣ#SG워너비\u200B #유야호\u200B #엠뚜루마뚜루\u200B MBC210417방송";
+                       Toast.makeText(getActivity().getApplicationContext(), "arr"+roominfoarr , Toast.LENGTH_LONG).show();
+                      Toast.makeText(getActivity().getApplicationContext(), "length"+roominfo_size , Toast.LENGTH_LONG).show();
+        for(int i = 0; i < roominfo_size; i++){
+            String title = roominfoarr.getJSONObject(i).getString("title");
             if(Build.VERSION.SDK_INT >= Build.VERSION_CODES.N) {
                 title = String.valueOf(Html.fromHtml(title, Html.FROM_HTML_MODE_COMPACT));
             } else {
                 title = String.valueOf(Html.fromHtml(title));
             }
             listPlVideoName.add(title);
-            listPlPublisher.add("엠뚜루마뚜루 : MBC 공식 종합 채널");
-            listPlVideoId.add("wV81QXfN5O8");
-            listPlThumbnailURL.add("https://i.ytimg.com/vi/wV81QXfN5O8/hqdefault.jpg");
+            listPlPublisher.add(roominfoarr.getJSONObject(i).getString("publisher"));
+            listPlVideoId.add(roominfoarr.getJSONObject(i).getString("videoId"));
+            listPlThumbnailURL.add(roominfoarr.getJSONObject(i).getString("thumbnailUrl"));
         }
 
-        for(int i = 0; i < 1; i++){
-            String title = "[놀면 뭐하니?] 편집자가 연속 듣기 하고 싶어 모아온,,, MSG워너비 12인 참가자 블라인드 무대 한번에 몰아보기!!ㅣ#MSG워너비\u200B #유야호\u200B MBC210501방송";
-            if(Build.VERSION.SDK_INT >= Build.VERSION_CODES.N) {
-                title = String.valueOf(Html.fromHtml(title, Html.FROM_HTML_MODE_COMPACT));
-            } else {
-                title = String.valueOf(Html.fromHtml(title));
-            }
-            listPlVideoName.add(title);
-            listPlPublisher.add("엠뚜루마뚜루 : MBC 공식 종합 채널");
-            listPlVideoId.add("wV81QXfN5O8");
-            listPlThumbnailURL.add("https://i.ytimg.com/vi/_Io-eJJyIWA/hqdefault.jpg");
-        }
-
-        for(int i = 0; i < listPlVideoName.size(); i++){
+        for(int i = 0; i < roominfo_size; i++){
             PlaylistItem data = new PlaylistItem();
             //System.out.println("정보: "+listPlVideoName.get(i)+" "+listPlPublisher.get(i));
             data.setPlVideoName(listPlVideoName.get(i));
@@ -133,6 +141,45 @@ public class PlaylistFragment extends Fragment {
             data.setPlRoomCode(roomCode);
 
             plItemList.add(data);
+
         }
+
+
+                    } catch (JSONException e) {
+                        e.printStackTrace();
+                    }
+                  //  Toast.makeText(getActivity().getApplicationContext(), "msg from server => title: " , Toast.LENGTH_LONG).show();
+                }, error -> {
+            Toast.makeText(getActivity().getApplicationContext(), "fail : msg from server", Toast.LENGTH_LONG).show();
+        });
+
+       requestQueue.add(jsonObjReq);
+    }
+    
+    private void postMedia(String roomCode, String videoTitle, String publisher, String thumbnailUrl, String videoId) {
+        String url = "http://3.37.36.38:3000/media";
+        RequestQueue requestQueue = Volley.newRequestQueue(getActivity().getApplicationContext());
+        requestQueue.start();
+
+        JSONObject params = new JSONObject();
+
+        try {
+            params.put("roomCode", roomCode);
+            params.put("videoTitle", videoTitle);
+            params.put("publisher", publisher);
+            params.put("thumbnailUrl", thumbnailUrl);
+            params.put("videoId", videoId);
+        } catch (JSONException e){
+            e.printStackTrace();
+        }
+
+        JsonObjectRequest jsonObjReq = new JsonObjectRequest(Request.Method.POST, url, params,
+                response -> {
+                    Toast.makeText(getActivity().getApplicationContext(), "msg from server : " + response, Toast.LENGTH_LONG).show();
+                }, error -> {
+            Toast.makeText(getActivity().getApplicationContext(), "fail : msg from server", Toast.LENGTH_LONG).show();
+        });
+
+        requestQueue.add(jsonObjReq);
     }
 }
